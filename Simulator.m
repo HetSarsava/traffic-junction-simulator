2cd656c09b860b37e11a314558fd755733a1689c
HetSarsava <hetsarsava2@gmail.com>
2025-11-19 16:17:10 +0530

    Car moving forward

--- Simulator.m
+++ Simulator.m
@@ -2,40 +2,58 @@ figure('Color',[0 0 0]);
 hold on;
 axis equal off;
 
-% --- 1. Setup Road and Lines ---
+% --- 1. Setup Static Background ---
 road_W_total = 10;
 road_H = 20;
 
-% Draw background (The black asphalt)
-rectangle('Position',[0 0 road_W_total road_H], 'FaceColor',[0.1 0.1 0.1], 'EdgeColor','none');
-
-% Define Lane Lines
-line_Left = 3;
-line_Right = 7;
+% Draw Road
+rectangle('Position',[0 0 road_W_total road_H], ...
+          'FaceColor',[0.1 0.1 0.1], 'EdgeColor','none');
 
 % Draw Lines
+line_Left = 3;
+line_Right = 7;
 plot([line_Left line_Left],[0 road_H],'w','LineWidth',3);
 plot([line_Right line_Right],[0 road_H],'w','LineWidth',3);
 
-% --- 2. Create Car Object ---
-% Calculate the width of the actua lane
-lane_width = line_Right - line_Left; % This equals 4
-
-% Define Car Dimensions
-car_W = 0.7 * lane_width; % 0.7 * 4 = 2.8
-car_L = 2 * car_W;        % 1:2 ratio = 5.6
-
-% Center the car in the lane
-lane_center = (line_Left + line_Right) / 2;
-
-car_x = lane_center - (car_W / 2);
-car_y = (road_H - car_L) / 2;
-
-% Draw the Car
-rectangle('Position', [car_x, car_y, car_W, car_L], ...
-          'FaceColor', 'b', 'EdgeColor', 'none');
-
-% Set view limits
 xlim([0 road_W_total]);
 ylim([0 road_H]);
+
+% --- 2. Setup Car ---
+lane_width = line_Right - line_Left;
+car_W = 0.7 * lane_width; 
+car_L = 2 * car_W;      
+
+lane_center = (line_Left + line_Right) / 2;
+car_x = lane_center - (car_W / 2);
+
+% Initial Y position
+current_y = 0; 
+
+% Create the Car Object
+hCar = rectangle('Position', [car_x, current_y, car_W, car_L], ...
+                 'FaceColor', 'b', 'EdgeColor', 'none');
+
+% --- 3. Physics Configuration ---
+velocity = 5;   % Speed: 5 units per second
+dt = 0.05;      % Time step: Update every 0.05 seconds (20 frames per second)
+
+% --- 4. Animation Loop ---
+% Using 'while' gives us more control than 'for'
+while current_y < road_H
+    
+    % Calculate new position based on speed and time
+    % Distance = Velocity * Time
+    current_y = current_y + (velocity * dt);
+    
+    % Update the car on screen
+    hCar.Position = [car_x, current_y, car_W, car_L];
+    
+    % Update the graph
+    drawnow;
+    
+    % Pause for the duration of the time step to maintain constant speed
+    pause(dt); 
+end
+
 hold off;
